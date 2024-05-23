dc4726b750ed1ca3018eb86c463e176d41d8177f
deacuradu <deacuradu@gmail.com>
2024-05-23 16:16:59 +0000

    first commit idx set up API

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Coins from './components/Coins';
+import Coinitem from './components/Coinitem';
+
+function App() {
+  const [coins, setCoins] = useState([])
+
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=10';
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data)
+      console.log(response.data[0])
+    }).catch(error => {
+      console.log(error)
+    })
+
+  }, [])
+
+
+  return (
+    <>
+      <Coins coins={coins}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Coins.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Coins = (props) => {
+  return (
+    <div className="container">
+        <div>
+            <div className="heading">
+                <p>#</p>
+                <p className="coin-name">Coin</p>
+                <p>Price</p>
+                <p>24h Change</p>
+                <p className="hide-mobile">Volume</p>
+                <p className="hide-mobile">Market Cap</p>
+            </div>
+
+            
+        </div>
+    </div>
+  )
+}
+
+export default Coins
