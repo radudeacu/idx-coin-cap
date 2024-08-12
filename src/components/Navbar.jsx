9fc001a666601f8f835a0496f72eed6138fc9b94
deacuradu <deacuradu@gmail.com>
2024-08-12 14:57:51 +0000

    changed made icon + its css | not final

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import {FaCoins} from 'react-icons/fa'
+// import {FaCoins} from 'react-icons/fa'
+// import { BsCcCircle } from "react-icons/bs"
+import { BsCcCircleFill } from "react-icons/bs";
 import './Navbar.css'
 
 const Navbar = () => {
   return (
     <div className="navbar">
-        <FaCoins className='icon' />
+        <BsCcCircleFill className='icon' />
         <h1> Coin <span className='blue'>Cap</span></h1>
     </div>
   )
