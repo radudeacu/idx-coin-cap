77d57d53b8f6d4141d943765e259525dd7afb917
deacuradu <deacuradu@gmail.com>
2024-08-23 15:27:39 +0000

    properly display the coin data for each coin

--- src/pages/Coin.jsx
+++ src/pages/Coin.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import {useParams} from 'react-router-dom'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import './Coin.css'
 
 const Coin = () => {
+
+  const params = useParams()
+  const [coin, setCoin] = useState({})
+
+  const url = `https://api.coingecko.com/api/v3/coins/${params.coinId}`
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoin(response.data)
+      console.log(response.data)
+    }).catch(error => {
+      console.log(error)
+    })
+  }, [])
+
   return (
-    <div>Coin</div>
+    <div>
+      <h1>{coin.id}</h1>
+    </div>
   )
 }
 
